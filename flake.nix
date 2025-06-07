1f2e1a06690ae4f194e25e6d6ab894b286fb092b
Henry <cootshk@duck.com>
2025-06-07 16:11:23 -0500

    node 16

--- flake.nix
+++ flake.nix
@@ -5,16 +5,26 @@
     node16_nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, node16_nixpkgs, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        nodepkgs = import node16_nixpkgs { inherit system; };
         pkgs = import nixpkgs { inherit system; };
-        nodepkgs = import inputs.node16_nixpkgs { inherit system; };
+        buildNode = nodepkgs.callPackage
+          "${node16_nixpkgs}/pkgs/development/web/nodejs/nodejs.nix" {
+            python = pkgs.python310;
+          };
+        node16 = buildNode {
+          enableNpm = true;
+          version = "16.14.2";
+          sha256 = "sha256-6SLiFcxo61+U0z6KC2HiyGO3cxzIYAq5VdOCLakP+NE=";
+        };
         buildInputs = with pkgs; [
-          nodepkgs.nodejs-16_x
-          yarn
-          python311Full
-          python311Packages.pygobject3
+          # nodepkgs.nodejs-16_x
+          node16
+          nodepkgs.yarn
+          python310Full
+          python310Packages.pygobject3
           xorg.libX11
           glib
           nss
